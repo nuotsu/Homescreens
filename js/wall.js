6b530975fe4f63e3a8c261f909d73f5fa4fb2d6f
nuotsu <xzmaru@gmail.com>
2018-02-20 19:47:50 -0800

    v5.0.1

--- js/wall.js
+++ js/wall.js
@@ -1,26 +1,32 @@
-var size = ['all', 'iS', 'iM', 'iL', 'iX', 'a']
+$(function() {
+    var size = ['all', 'iS', 'iM', 'iL', 'iX', 'a']
 
-$('article.switch').each(function() {
-    for (var i in size)
-        $(this).append(`
-            <iframe src="${$(this).attr('art')}.html?art=${size[i]}"></iframe>
-        `)
-})
-
-var indx = 0
-var interval = 15 // seconds
-var fade = 2 // seconds
-
-switchArt(0)
-
-setInterval(function() {
-    indx++; if (indx >= size.length) indx = 0
-    switchArt(indx)
-}, 1000 * interval)
-
-function switchArt(index) {
     $('article.switch').each(function() {
-        $(this).find('iframe').fadeOut(1000*fade)
-        $(this).find('iframe').eq(index).fadeIn(1000*fade)
+        for (var i in size)
+            $(this).append(`
+                <iframe src="${$(this).attr('art')}.html?art=${size[i]}"></iframe>
+            `)
     })
-}
+
+    var indx = 0
+    var interval = 15 // seconds
+    var fade = 2 // seconds
+
+    switchArt(0)
+
+    setInterval(function() {
+        indx++; if (indx >= size.length) indx = 0
+        switchArt(indx)
+    }, 1000 * interval)
+
+    function switchArt(index) {
+        $('article.switch.noFade').each(function() {
+            $(this).find('iframe').hide()
+            $(this).find('iframe').eq(index).show()
+        })
+        $('article.switch.fade').each(function() {
+            $(this).find('iframe').fadeOut(1000*fade)
+            $(this).find('iframe').eq(index).fadeIn(1000*fade)
+        })
+    }
+})
