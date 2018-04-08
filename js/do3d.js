38ac618883c9bc79b98f3b49dd12416c8d521c80
nuotsu <xzmaru@gmail.com>
2018-04-08 14:28:38 -0700

    v6.1.0.22

--- js/do3d.js
+++ js/do3d.js
@@ -46,8 +46,8 @@
         $('#y code').html(Math.round(e.gamma))  // Y --> e.gamma
         $('#z code').html(Math.round(e.alpha))  // Z --> e.alpha
 
-        document.getElementById('hs-3d').style.webkitTransform =
-        document.getElementById('hs-3d').style.transform =
+        document.getElementById('hs-3d-img').style.webkitTransform =
+        document.getElementById('hs-3d-img').style.transform =
             `rotateX(${e.beta}deg) ` +
             `rotateY(${e.gamma}deg) ` +
             `rotateZ(${e.alpha}deg)`;
